8f2426a01adeea88d9f539f36e62ccf922064ce0
lu1salmazan <luis_almazan-10@live.com>
2016-01-05 10:42:03 -0800

    Signed-off-by: lu1salmazan <luis_almazan-10@live.com>

---
+++ Class/HelloWord/main.cpp
@@ -0,0 +1,32 @@
+/* 
+     File:   main.cpp
+    Author: Luis Almazan
+    Created: on January 4, 2016, 10:18 Am
+    Purpose: Check out IDE
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+  
+ //Global Constant
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare an initialize variables
+    
+    //Input data
+    
+    //Calculate or map input to output
+    
+    //Output the result
+     cout<<"Hello World"<<endl;
+            
+    //Exit Stage Right        
+    return 0;
+}
+
