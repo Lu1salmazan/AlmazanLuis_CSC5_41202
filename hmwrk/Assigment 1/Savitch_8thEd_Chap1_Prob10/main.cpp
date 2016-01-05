990c1fdfb105f700bbe726ede53634ef76521569
lu1salmazan <luis_almazan-10@live.com>
2016-01-05 12:00:07 -0800

    Signed-off-by: lu1salmazan <luis_almazan-10@live.com>

---
+++ hmwrk/Assigment 1/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Almazan Luis
+ * Created: on January 5, 2016, 11:42 AM
+ * Purpose: Problem solving
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User library 
+
+//Global Constant
+ 
+// Function Prototype
+
+int main(int argc, char** argv) {
+//Declare and initialize variables
+    char letter= "X"
+    
+    //Output the results
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    count<<" "<<letter<<"   "<<letter<<endl;
+    count<<letter<<endl;
+    count<<letter<<endl;
+    count<<letter<<endl;
+    count<<letter<<endl;
+    count<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    return 0;
+}
+
