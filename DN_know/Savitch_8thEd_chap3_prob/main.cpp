a1faa2a141adf9a5ef91601075423425a0a72646
lu1salmazan <luis_almazan-10@live.com>
2016-01-19 12:10:46 -0800

    Signed-off-by: lu1salmazan <luis_almazan-10@live.com>

--- DN_know/Savitch_8thEd_chap3_prob/main.cpp
+++ DN_know/Savitch_8thEd_chap3_prob/main.cpp
@@ -1,19 +1,38 @@
 /* 
  * File:   main.cpp
- * Author: rcc
- *
- * Created on January 19, 2016, 11:46 AM
+ * Author: Luis Almazan
+ * Created on January 19, 2016, 8:35 AM
+ * Purpose: Menu
  */
 
-#include <cstdlib>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User  Libraries
 
+//Global Constant
+
+//Function Prototypes 
+
+//Execution Begin Here
+
+int main(int argc, char** argv) {
+    //Declare and Initialize variables 
+    float pi;       //display for pie Number
+    unsigned int nTerms=1,ndTerm;
+    
+    //Input
+    cout<<"Enter the number of terms deciered"<<endl;
+    cin>>ndTerm;
+    
+    //Calculate
+    if(nTerms)
+        
+    //Output the results
+        cout<<pi<<endl;
+    //Exit stage right
+    
     return 0;
 }
-
