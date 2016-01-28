e743d4a38f2914290c050746698cb2baf378429c
lu1salmazan <luis_almazan-10@live.com>
2016-01-28 10:35:58 -0800

    Signed-off-by: lu1salmazan <luis_almazan-10@live.com>

---
+++ MidTerm/Prob_2/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Luis Almazan
+ * Created on January 28, 2016, 10:22 AM
+ * Purpose: * per number of places
+ */
+
+#include <iostream>
+
+using namespace std;
+//System libraries
+
+//User libraries
+
+//Global Constants
+
+//Function prototypes
+int main(int argc, char** argv) {
+    //Declare Variables
+    int n1000s,n100s,n10,n1s; //Number of Places
+    float numIN;                //Number input by the user
+    
+    //Input
+    cout<<"Please Enter a number no more than you thousandths place."<<endl;
+    cin>>numIN;
+    
+    
+
+    
+    cout<<n1000s<<endl;
+    
+    return 0;
+}
+
